10d81c6fbd2bcec03fa62a4265bcd6f7aeacb0ca
winterdelta <w@winterdelta.com>
2022-01-08 14:54:40 +0000

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import useSWR from 'swr'
-import Person from '../components/Person'
+import useSWR from 'swr';
+import Person from '../components/Person';
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
-  const { data, error } = useSWR('/api/people', fetcher)
+  const { data, error } = useSWR('/api/people', fetcher);
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
-    <ul>
-      {data.map((p, i) => (
-        <Person key={i} person={p} />
-      ))}
-    </ul>
-  )
+    <>
+      <h1>Hi</h1>
+      <ul>
+        {data.map((p, i) => (
+          <Person key={i} person={p} />
+        ))}
+      </ul>
+    </>
+  );
 }
